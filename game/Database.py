156a2808841d18c1a909e09ff68423523a98e4ec
Ayman Rabaya <ayman.waleed@harri.com>
2022-12-19 12:48:01 +0200

    create singleton database connection class

--- game/Database.py
+++ game/Database.py
@@ -5,14 +5,10 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 class DB:
     db_session = None
 
-    def __init__(self):
-        engine = create_engine("mysql://root:Ayman1234@127.0.0.1/chess_game", echo=False, future=True)
-        Session = sessionmaker(bind=engine)
-        db_session = Session()
+    def __new__(cls):
+        if cls.db_session is None:
+            engine = create_engine("mysql://root:Ayman1234@127.0.0.1/chess_game", echo=False, future=True)
+            Session = sessionmaker(bind=engine)
+            cls.db_session = Session()
+        return cls.db_session
 
-    @staticmethod
-    def getInstance():
-        if DB.db_session is None:
-            db_session = DB()
-
-        return db_session
--- game/components.py
+++ game/components.py
@@ -9,8 +9,8 @@ class UpcomingTournaments:
 
     @staticmethod
     def get_upcoming_tournaments():
-        engine = create_engine("mysql://root:Ayman1234@127.0.0.1/chess_game", echo=False, future=True)
-        Session = sessionmaker(bind=engine)
-        db_session = Session()
+        #engine = create_engine("mysql://root:Ayman1234@127.0.0.1/chess_game", echo=False, future=True)
+        #Session = sessionmaker(bind=engine)
+        db_session = DB()
         return db_session.query(Tournament).filter(Tournament.date > datetime.datetime.now()).all()
 
